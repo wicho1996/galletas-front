a979349dc22a92c8ae35c00ed8ab3d0105dc7be7
wicho1996 <63920120+wicho1996@users.noreply.github.com>
2022-06-25 10:39:50 -0500

    configuracion base de datos

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -26,13 +26,6 @@ import { Seguimiento } from 'views/Reportes';
 // dashboard routing
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
 
-// utilities routing
-const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
-const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
-const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
-const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
-const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
-
 // ==============================|| MAIN ROUTING ||============================== //
 const elements = {
     ctlPst: Puestos,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,42 +1,34 @@
 import { useRoutes } from 'react-router-dom';
 
 // routes
-import MainRoutes from './MainRoutes';
+// import MainRoutes from './MainRoutes';
 // import AuthenticationRoutes from './AuthenticationRoutes';
-import config from 'config';
+// import config from 'config';
 import { useContext, useEffect, useState } from 'react';
-// import LoginContext from 'Context/ContextLogin';
-// import axios from './utils/axios';
-import axiosSystem from "./utils/axiosSystem";
+import axiosSystem from "../utils/axiosSystem";
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
     // const context = useContext(LoginContext);
     const [rutas, setRutas] = useState([]);
+
+    const routes = {
+        getPages: axiosSystem("Login/getUsuarios")
+    };
+
+    const getRutas = () => {
+        routes.getPages(
+            (res) => {
+            setRutas(res);
+            console.log(res);
+            },
+            { dat: "Hola" }
+        );
+    };
+
     useEffect(() => {
-        // if (context.login?.token && !context.login?.usuario) {
-            // context.bloqueo(true);
-            // axios.defaults.headers.common = { authorization: context.login?.token };
-            axiosSystem("Login/getPaginas")
-                // .get('/usuario/TraerRutas')
-                // .then((response) => {
-                //     // context.bloqueo(false);
-                //     if (response.status == 200) {
-                //         setRutas(MainRoutes(response.data.rutas, response.data.info));
-                //         // context.cambioLogin({
-                //         //     ...context.login,
-                //         //     usuario: response.data.info
-                //         // });
-                //     }
-                // })
-                // .catch((error) => {
-                //     console.error(`error ${error}`);
-                //     // context.cerrarLogin();
-                // });
-        // } 
-        // else 
-        // if (!context.login?.token) setRutas(AuthenticationRoutes);
-    }, []);
-    return useRoutes([rutas], config.basename);
+        getRutas();
+    }, [])
+    // return useRoutes([rutas], config.basename);
 }
